df98c517d8bff374f19c0104378e290679bce657
Hugo Romero <hugomrj@gmail.com>
2024-09-09 10:21:27 -0400

    2024set9:1021

--- wsgi.py
+++ wsgi.py
@@ -20,9 +20,19 @@ if not app.debug:
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
+
+# Configurar el formato del logging
+handler = logging.StreamHandler()  # Puedes usar FileHandler si quieres escribir en un archivo
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
 # Registrar información sobre la configuración y la base de datos
-app.logger.info(f"Configuración activa: {app.config.get('FLASK_ENV', 'No FLASK_ENV configured')}")
+app.logger.info(f"Config wsgi")
+app.logger.info(f"Configuración activa: {os.getenv('FLASK_ENV', 'No FLASK_ENV configured')}")
 app.logger.info(f"URL de la base de datos: {app.config.get('SQLALCHEMY_DATABASE_URI', 'No URL configured')}")
+
 
 # Gunicorn usa el archivo WSGI para servir la aplicación
 if __name__ == "__main__":
